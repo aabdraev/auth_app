093bb8e8be4b0ad0b08a6dd53a7ab453952161ec
aabdraev <ataiabdraev@gmail.com>
2023-06-12 20:11:05 +0600

    small fixes

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials, logOut } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://auth-api.onrender.com',
+    baseUrl: 'https://auth-app-api-catm.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
-    'https://www.google.com',
+    'https://auth-app-04te.onrender.com/',
 ]
 
 module.exports = allowedOrigins
