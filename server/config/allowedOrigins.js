bfb3c439215ec2105120a0eaec44e2199e848986
aabdraev <ataiabdraev@gmail.com>
2023-06-12 20:21:39 +0600

    small fixes

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'https://auth-app-04te.onrender.com/',
+    "https://auth-project-xa39.onrender.com"
 ]
 
 module.exports = allowedOrigins
